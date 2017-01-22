2439ba08db58c01f951e6fc10186c046214ca86f
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-22 11:59:08 -0500

    Search bar fix

--- public/js/ajaxgit.js
+++ public/js/ajaxgit.js
@@ -104,5 +104,23 @@ $('#add-topic').on('click', function(){
 			}
 		});
 	});
+
+	$("#searchterm").keyup(function(e){
+		var q = $("#searchterm").val();
+		$.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+		{
+			srsearch: q,
+			action: "query",
+			list: "search",
+			format: "json"
+		},
+		function(data) {
+			$("#results").empty();
+			$("#results").append("<b>" + q + "</b>");
+			$.each(data.query.search, function(i,item){
+				$("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a>" + item.snippet + "</div>");
+			});
+		});
+	});
 });
 
